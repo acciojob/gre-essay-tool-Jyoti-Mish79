388a006f649353493081f8828c6df9359e4c57c4
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2023-02-08 10:47:54 +0530

    solo

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+  const wordCountDisplay = document.getElementById("wordCount");
+
+  textbox.addEventListener("input", function() {
+    const wordCount = textbox.value.split(" ").length;
+    wordCountDisplay.innerText = wordCount === 0 ? "0" : wordCount;
+  });
+  
